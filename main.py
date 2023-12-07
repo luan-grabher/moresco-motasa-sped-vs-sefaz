5c63102725b77fe2aa4e72950635176ac279eec6
luangrabher <luanggcontato@gmail.com>
2023-12-06 21:30:47 -0300

    feat - atualizado requirements.txt. criado instalador

--- main.py
+++ main.py
@@ -1,17 +1,3 @@
-#[X]:Usuario escolhe arquivo sped
-#[X]: Usuario escolhe arquivo sefaz
-#[x]:  (Opcional) Usuario pode escolher mais de uma pasta e o robo seelciona [ ]s arquivos .txt que tenham as colunas do sefaz separadas por ; e junta [ ]s em um array só. (Caso fizer deixa em stand by comentado e pergunta para elas se é assim ou não)
-#[x]: Extrai dados do sped → notas + produtos da nota
-#[x]: Extrai dados do sefaz
-#[x]: Lista de notas do sped que não existem no sefaz
-#[x]: Lista de notas no sefaz que nao existem no sped
-#[x]: Lista de notas no sefaz e no sped
-#[x]: Comparar valores totais das notas sefaz com soma do icms dos produtos do sped conforme lista de notas que estao nos dois arquivos
-#[x]: Relatório em HTML com os comparativos
-#[x]: Salvar relatório HTML no desktop e abrir o arquivo após a conclusão do programa
-#[ ]: Testes
-#[ ]: Criação de instalador
-
 import os
 
 os.system('python -m pip install --upgrade pip')
